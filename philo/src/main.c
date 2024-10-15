16ad18036e05964cf7e7cf03aa294bf749db0c2e
samueltingg <samuelting7@gmail.com>
2024-10-15 09:43:13 +0800

    feat: half in setting up structure with all pthread functions

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,12 +6,38 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:59:13 by sting             #+#    #+#             */
-/*   Updated: 2024/10/14 15:04:29 by sting            ###   ########.fr       */
+/*   Updated: 2024/10/15 09:42:49 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+void eat() {
+
+}
+
+void sleep() {
+
+}
+
+void think() {
+
+}
+
+void *run_routine(void *ptr) {
+
+
+
+}
+
+
+// Todo: parse arguments
 int main(void) {
-	printf("hello!");	
+	pthread_mutex_t mutex;
+
+	pthread_t thread1;
+
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_create(&thread1, NULL, run_routine, )
 }
